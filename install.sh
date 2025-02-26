5c9b55d3a36459c52890f3d2b335021ffdf9f8d1
mnansary <nazmuddoha.ansary.28@gmail.com>
2025-02-26 17:13:47 +0600

    Yolox-ByteTrack base

---
+++ install.sh
@@ -0,0 +1,9 @@
+#pytorch
+pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
+# bytetrack reqs
+pip install -r requirements.txt
+pip install -e dependencies/ByteTrack/
+# tensor-rt
+pip install nvidia-pyindex
+pip install nvidia-tensorrt==99.0.0
+pip install git+https://github.com/NVIDIA-AI-IOT/torch2trt.git
